71fc3cf35a95ac14f41ff4d8c5bb3cf3ada785aa
Juan Ramón del Caño Vega <delcanovega@gmail.com>
2024-08-27 20:20:06 +0200

    Use rounded font

--- Remmi/RemmiApp.swift
+++ Remmi/RemmiApp.swift
@@ -16,6 +16,7 @@ struct RemmiApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .font(.system(.body, design: .rounded))
         }
         .modelContainer(for: Category.self)
     }
